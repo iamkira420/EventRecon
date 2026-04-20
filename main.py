43fe927cdf05ec7f2b72ad778853463c073a07c1
iamkira420 <lmboniswa01@gmail.com>
2026-04-20 19:48:14 +0200

    basic main - playing with items

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+
+items = []
+
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
+
